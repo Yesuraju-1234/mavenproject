0bac2ec9b81a6eb14923baf403d446fcdb8fe95f
Yesuraju-1234 <vallepuyesuraju11@gmail.com>
2026-05-21 05:57:26 +0530

    Corre.java
    Fix main method declaration and remove invalid characters.

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -6,7 +6,9 @@ package demo;
  */
 public class App 
 {
-    public static void main( String[] args )
+    {vgfd}
+    public stat
+ic void main( String[] args )
     {
         System.out.println( "Hello World! from CI CD task package hi from demo by jenkins" );
     }
